dbf3fa2b842dac5b01eb2179130ef45393d19d48
vemulamanoj17 <vemulamanoj50@gmail.com>
2025-03-25 18:03:55 +0530

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import express from "express"
+import { PrismaClient } from "@prisma/client"
+const app=express()
+const prismaClient = new PrismaClient();
+
+app.get("/",async(req,res) => {
+    const data=await prismaClient.user.findMany()
+    res.json({
+        data
+    })
+})
+app.post("/",async(req,res)=>{
+    await prismaClient.user.create({
+        data:{
+            username:Math.random().toString(),
+            password:Math.random().toString()
+        }
+    })
+    res.json({
+        "message":"post endpoint"
+    })
+})
+app.listen(3000);
